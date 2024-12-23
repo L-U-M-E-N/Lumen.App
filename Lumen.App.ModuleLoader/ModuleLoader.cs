924528c33d6387affd479cbb4aeecf131b2f60ba
Elanis <elanis@hotmail.com>
2024-12-23 18:37:51 +0100

    ModuleLoader - Simple Implementation

---
+++ Lumen.App.ModuleLoader/ModuleLoader.cs
@@ -0,0 +1,45 @@
+﻿using Lumen.Modules.Sdk;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using System.Reflection;
+
+namespace Lumen.App.ModuleLoader {
+    public static class ModuleLoaderHelper {
+        public static LumenModuleRunsOnFlag RunsOn { get; set; }
+
+        public static IEnumerable<Assembly> LoadModules(this IServiceCollection services) {
+            var DLLlist = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory)
+                .Where(x => x.EndsWith(".dll"));
+            var modulesAssemblies = new List<Assembly>();
+
+            foreach (var file in DLLlist) {
+                var currentAssembly = Assembly.LoadFrom(file);
+                IEnumerable<Type> modules = currentAssembly.ExportedTypes.Where(x => x.IsSubclassOf(typeof(LumenModuleBase)));
+
+                foreach (var module in modules) {
+                    services.AddTransient<LumenModuleBase>(x => {
+                        var instance = Activator.CreateInstance(
+                            module,
+                            [
+                                RunsOn,
+                                    x.GetRequiredService<ILogger<LumenModuleBase>>()
+                            ]
+                        );
+
+                        return instance is null
+                            ? throw new ArgumentNullException($"Cannot instanciate the current module: {module.Name}")
+                            : (LumenModuleBase)instance;
+                    });
+                }
+
+                if (modules.Any()) {
+                    modulesAssemblies.Add(currentAssembly);
+                }
+            }
+
+            return modulesAssemblies;
+        }
+    }
+}
