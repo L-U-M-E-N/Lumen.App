23e2a3cd81b690e3a7f232e0c2add9d389ce3792
Elanis <elanis@hotmail.com>
2024-12-29 18:30:18 +0100

    WebAPI - Setup services & Db

--- Lumen.App.ModuleLoader/ModuleLoader.cs
+++ Lumen.App.ModuleLoader/ModuleLoader.cs
@@ -9,7 +9,7 @@ namespace Lumen.App.ModuleLoader {
     public static class ModuleLoaderHelper {
         public static LumenModuleRunsOnFlag RunsOn { get; set; }
 
-        public static IEnumerable<Assembly> LoadModules(this IServiceCollection services) {
+        public static IEnumerable<Assembly> LoadModules(this IServiceCollection services, string connectionString) {
             var DLLlist = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory)
                 .Where(x => x.EndsWith(".dll"));
             var modulesAssemblies = new List<Assembly>();
@@ -19,7 +19,7 @@ namespace Lumen.App.ModuleLoader {
                 IEnumerable<Type> modules = currentAssembly.ExportedTypes.Where(x => x.IsSubclassOf(typeof(LumenModuleBase)));
 
                 foreach (var module in modules) {
-                    services.AddTransient<LumenModuleBase>(x => {
+                    services.AddScoped<LumenModuleBase>(x => {
                         var instance = Activator.CreateInstance(
                             module,
                             [
@@ -28,10 +28,16 @@ namespace Lumen.App.ModuleLoader {
                             ]
                         );
 
-                        return instance is null
-                            ? throw new ArgumentNullException($"Cannot instanciate the current module: {module.Name}")
-                            : (LumenModuleBase)instance;
+                        if (instance is null) {
+                            throw new ArgumentNullException($"Cannot instanciate the current module: {module.Name}");
+                        }
+
+                        var typedInstance = (LumenModuleBase)instance;
+
+                        return typedInstance;
                     });
+
+                    module.GetMethod(nameof(LumenModuleBase.SetupServices), BindingFlags.Static | BindingFlags.Public)?.Invoke(null, new object[] { RunsOn, services, connectionString });
                 }
 
                 if (modules.Any()) {
--- Lumen.App.WebAPI/Program.cs
+++ Lumen.App.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 using Lumen.App.ModuleLoader;
+using Lumen.Modules.Sdk;
+
+using Microsoft.EntityFrameworkCore;
 
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -7,16 +10,17 @@ namespace Lumen.App.WebAPI;
 public class Program {
     protected Program() { }
 
-    public static void Main(string[] args) {
+    public static async Task Main(string[] args) {
         ModuleLoaderHelper.RunsOn = Lumen.Modules.Sdk.LumenModuleRunsOnFlag.API;
 
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        var modulesAssemblies = builder.Services.LoadModules();
+        var modulesAssemblies = builder.Services.LoadModules("CONNECTIONSTRING"); // TODO: Config
 
         var mvcBuilder = builder.Services.AddControllers();
         foreach (var module in modulesAssemblies) {
+            // Web controllers
             mvcBuilder.AddApplicationPart(module);
         }
         mvcBuilder.AddControllersAsServices();
@@ -24,6 +28,18 @@ public class Program {
         builder.Services.AddOpenApi();
 
         var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope()) {
+            var modulesList = scope.ServiceProvider.GetServices<LumenModuleBase>();
+            foreach (var module in modulesList) {
+                var dbContext = scope.ServiceProvider.GetService(module.GetDatabaseContextType());
+                if (dbContext is null) {
+                    throw new NullReferenceException(nameof(dbContext));
+                }
+                var typedDbContext = (DbContext)dbContext;
+                await typedDbContext.Database.MigrateAsync();
+            }
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment()) {
